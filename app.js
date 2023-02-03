995ebcde0381879c07886657ac41626428e8a979
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-03 06:09:18 +0000

    mouse

--- app.js
+++ app.js
@@ -8,15 +8,15 @@
 //   });
 // });
 
-$(function(){
-  $('.box1').slideDown(function(){
-   $('.box1').css({
-     'background-color': '#0000FF',
-     'width' : '200px',
-     'height': '100px'
-   }).slideUp();   
-  });
-});
+// $(function(){
+//   $('.box1').slideDown(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'width' : '200px',
+//     'height': '100px'
+//   }).slideUp();   
+//   });
+// });
 
 // $(function (){
 //   $('.box1').slideDown(function (){
@@ -27,3 +27,12 @@ $(function(){
 //     }).slideUp();
 //   });
 // });
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
+  });
+});
